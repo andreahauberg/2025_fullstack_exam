3cf55027807f7735b6446c844838d27f779cbd2a
Andrea <132267442+andreahauberg@users.noreply.github.com>
2025-12-02 21:50:32 +0100

    temp entrypoint

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 set -e
 cd /var/www/html
-# Kør migrations før Apache startes
-php artisan migrate --force
-# Opret storage symlink (idempotent)
-if [ ! -L public/storage ]; then
-    php artisan storage:link
-fi
-# Optionelt seed (sæt SEED_ON_BOOT=1 i miljøvariabler for at køre seedere)
-if [ "${SEED_ON_BOOT}" = "1" ]; then
-    php artisan db:seed --force
-fi
-# Start Apache
+# Start Apache uden migrations
 exec "$@"
+
+
+# #!/bin/sh
+# set -e
+# cd /var/www/html
+# # Kør migrations før Apache startes
+# php artisan migrate --force
+# # Opret storage symlink (idempotent)
+# if [ ! -L public/storage ]; then
+#     php artisan storage:link
+# fi
+# # Optionelt seed (sæt SEED_ON_BOOT=1 i miljøvariabler for at køre seedere)
+# if [ "${SEED_ON_BOOT}" = "1" ]; then
+#     php artisan db:seed --force
+# fi
+# # Start Apache
+# exec "$@"
