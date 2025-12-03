84b7eb4637003ad9ef7e8244065304d1e209e639
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2025-12-03 20:26:07 +0100

    allowed origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,10 +2,18 @@
 
 return [
     'paths' => ['api/*'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000')),
+
+    'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
+
     'supports_credentials' => false,
 ];
