9e3459900364f03658e79768ea639db31da30c80
Andrea <132267442+andreahauberg@users.noreply.github.com>
2025-12-03 21:50:28 +0100

    cors

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,9 +20,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/test', function () {
+    return response()->json(['message' => 'CORS works!']);
+});
+
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/login', [AuthController::class, 'login']); 
-
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/posts', [PostController::class, 'index']);
