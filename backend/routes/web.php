96a30e9fe849eb6a1e215eb00de9b3ef63fc6c9a
Andrea <132267442+andreahauberg@users.noreply.github.com>
2025-12-03 21:03:17 +0100

    web changed

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 
+// Dine API-routes
 Route::get('/', function () {
     return response()->json(['message' => 'API is running']);
 });
@@ -9,3 +10,7 @@ Route::get('/test', function () {
     return response()->json(['message' => 'Test route works!']);
 });
 
+// Catch-all route for React (placér den sidst)
+Route::get('/{any}', function () {
+    return file_get_contents(public_path('react/index.html'));
+})->where('any', '^(?!api).*$');
